fda128775e887be0bf9d31bcc79a50b7f855b36c
piioni <rangelc.juane@gmail.com>
2024-11-25 20:46:20 +0100

    Added numeroAmigo exercise

---
+++ Programas/ejercicioNumeroAmigos/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 0; i<=3000; i++){
+            for (int j=0; j<=3000; j++){
+                if (NumerosAmigos(i, j)){
+                    System.out.println(i+ " y " + j+ " Son numeros amigos" );
+                }
+            }
+        }
+    }
+
+    private static boolean NumerosAmigos(int numero1, int numero2) {
+        int suma1 = 0;
+        int suma2 = 0;
+
+        for (int i = 1; i < numero1; i++) {
+            if (numero1 % i == 0) {
+                suma1 += i;
+            }
+        }
+        for (int i = 1; i < numero2; i++) {
+            if (numero2 % i == 0) {
+                suma2 += i;
+            }
+        }
+
+        return suma1 == numero2 && suma2 == numero1;
+    }
+}
