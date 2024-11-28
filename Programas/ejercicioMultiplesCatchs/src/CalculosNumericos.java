94ce317010a29f7325f99d9accc5b34d021541e8
piioni <rangelc.juane@gmail.com>
2024-11-28 21:22:56 +0100

    Modify calculos numericos exercise, added a calcularRaices method, calcular puntos medio de una recta y calcular pendiente

--- Programas/ejercicioMultiplesCatchs/src/CalculosNumericos.java
+++ Programas/ejercicioMultiplesCatchs/src/CalculosNumericos.java
@@ -3,7 +3,7 @@ public class CalculosNumericos {
     public static void calcularLogaritmoNeperiano(double valor) {
         if (valor == (double) valor) {
             if (valor > 0) {
-                System.out.println("El logaritmo Neperiano de: " + valor + " es: " +  Math.log(valor));
+                System.out.println("El logaritmo Neperiano de: " + valor + " es: " + Math.log(valor));
             } else throw new ArithmeticException();
         } else throw new IllegalArgumentException();
     }
@@ -11,8 +11,48 @@ public class CalculosNumericos {
     public static void calcularRaizCuadrada(double valor) {
         if (valor == (double) valor) {
             if (valor > 0) {
-                System.out.println("La raiz cuadrada de: " + valor + " es: " +  Math.sqrt(valor));
+                System.out.println("La raiz cuadrada de: " + valor + " es: " + Math.sqrt(valor));
             } else throw new ArithmeticException();
         } else throw new IllegalArgumentException();
+    }
+
+    public static void calcularPendiente(double y2, double y1, double x2, double x1) {
+        if (y1 == (double) y1 && y2 == (double) y2 && x2 == (double) x2 && x1 == (double) x1) {
+            if (x2 - x1 != 0) {
+                double pendiente = (y2 - y1) / (x2 - x1);
+                System.out.println("La pendiente de la recta con los puntos: " + "(" + y2 + "," + y1 + ")" + " y " +
+                        "(" + x2 + "," + x1 + ")" + "\n" + "Es: " + pendiente);
+            } else throw new ArithmeticException();
+        } else throw new IllegalArgumentException();
+    }
+
+    public static void calcularPuntoMedioRecta(double y2, double y1, double x2, double x1) {
+        if (y1 == (double) y1 && y2 == (double) y2 && x2 == (double) x2 && x1 == (double) x1) {
+            double puntoMedioRecta = ((x2 +x1) / 2) + ((y2 + y1)/2);
+            System.out.println("El punto media de la recta con los puntos: " + "(" + y2 + "," + y1 + ")" + " y " +
+                    "(" + x2 + "," + x1 + ")" + "\n" + "Es: " + puntoMedioRecta);
+        } else throw new IllegalArgumentException();
+    }
+
+    public static void calcularRaices(double a, double b, double c){
+        if (a == (double) a && b == (double) b && c == (double) c) {
+            if (a == 0) throw new ArithmeticException();
+            else if (b*b - 4*a*c < 0) throw new ArithmeticException();
+            else {
+                double primera_raiz = (-b + Math.sqrt(b*b - 4*a*c))/2*a ;
+                double segunda_raiz = (-b - Math.sqrt(b*b - 4*a*c))/2*a ;
+                System.out.println("La primera raiz de la ecuacion" + a+"x^2 + " + b+"x + " + c + "\n" +
+                        "Primera Raiz: " + primera_raiz + "\n" +
+                        "Segunda Raiz: " + segunda_raiz);
+            }
+        }
+    }
+
+    public static void calcularBase2(int numero){
+        StringBuilder numero_base2 = new StringBuilder();
+        while (numero != 2) {
+//            numero_base2 +=(String) numero % 2;
+            numero /= 2;
+        }
     }
 }
--- Programas/ejercicioMultiplesCatchs/src/Main.java
+++ Programas/ejercicioMultiplesCatchs/src/Main.java
@@ -1,4 +1,3 @@
-import javax.tools.OptionChecker;
 import java.util.Scanner;
 
 public class Main {
@@ -9,9 +8,13 @@ public class Main {
         do {
             System.out.println("""
                 Que quieres realizar?
+                0. Salir del programa
                 1. Calcular el Logaritmo Neperiano
                 2. Calular la Raiz Cuadrada.
-                0. Salir del programa""");
+                3. Calcular la pendiente entre 2 puntos.
+                4. Calcular el punto medio.
+                5. Calcular las raices de una ecuación cuadratica.
+                """);
             String operacion = sc.nextLine();
             switch (operacion) {
                 case "0":
@@ -22,8 +25,6 @@ public class Main {
                         System.out.println("Introduzca el numero del que quiere calcular el Logaritmo Neperiano ");
                         double numero = sc.nextDouble();
                         CalculosNumericos.calcularLogaritmoNeperiano(numero);
-                        opcion = 0;
-                        break;
                     } catch (IllegalArgumentException e1) {
                         System.out.println("No haz introducido un numero valido");
                         sc.nextLine();
@@ -34,13 +35,13 @@ public class Main {
                         System.out.println("Error en la operation");
                         sc.nextLine();
                     }
+                    sc.nextLine();
+                    break;
                 case "2":
                     try {
                         System.out.println("Introduzca el numero del que quiere realizar la raiz cuadrada");
                         double numero = sc.nextDouble();
                         CalculosNumericos.calcularRaizCuadrada(numero);
-                        opcion = 0;
-                        break;
                     } catch (IllegalArgumentException e1) {
                         System.out.println("No haz introducido un numero valido");
                         sc.nextLine();
@@ -51,8 +52,73 @@ public class Main {
                         System.out.println("Error en la operation");
                         sc.nextLine();
                     }
+                    sc.nextLine();
+                    break;
+                case "3":
+                    try {
+                        System.out.println("Te voy a preguntar 4 cordenadas, Y2, Y1, X2 y X1");
+                        System.out.println("Introduce Y2:");
+                        double y2 = sc.nextDouble();
+                        System.out.println("Introduce Y1:");
+                        double y1 = sc.nextDouble();
+                        System.out.println("Introduce X2:");
+                        double x2 = sc.nextDouble();
+                        System.out.println("Introduce X1:");
+                        double x1 = sc.nextDouble();
+                        CalculosNumericos.calcularPendiente(y2, y1, x2, x1);
+                    } catch (IllegalArgumentException e1) {
+                        System.out.println("No haz introducido un numero valido");
+                    } catch (ArithmeticException e2) {
+                        System.out.println("La resta de X2 - X1 no puede ser igual a 0");
+                    } catch (Exception e3) {
+                        System.out.println("Error en la operation");
+                    }
+                    sc.nextLine();
+                    break;
+                case "4":
+                    try {
+                        System.out.println("Te voy a preguntar 4 cordenadas, Y2, Y1, X2 y X1");
+                        System.out.println("Introduce Y2:");
+                        double y2 = sc.nextDouble();
+                        System.out.println("Introduce Y1:");
+                        double y1 = sc.nextDouble();
+                        System.out.println("Introduce X2:");
+                        double x2 = sc.nextDouble();
+                        System.out.println("Introduce X1:");
+                        double x1 = sc.nextDouble();
+                        CalculosNumericos.calcularPuntoMedioRecta(y2, y1, x2, x1);
+                    } catch (IllegalArgumentException e1) {
+                        System.out.println("No haz introducido un numero valido");
+                    } catch (Exception e3) {
+                        System.out.println("Error en la operation");
+                    }
+                    sc.nextLine();
+                    break;
+                case "5":
+                    try{
+                        System.out.println("Te voy a preguntar el valor de a, b y c, tomando como ejemplo una ecuacion: ax^2 + bx +c");
+                        System.out.println("Introduce a: ");
+                        double a = sc.nextDouble();
+                        System.out.println("Introduce b: ");
+                        double b = sc.nextDouble();
+                        System.out.println("Introduce c: ");
+                        double c = sc.nextDouble();
+                        CalculosNumericos.calcularRaices(a, b, c);
+                    } catch (IllegalArgumentException e1){
+                        System.out.println("No haz introducido un numero valido");
+                        sc.nextLine();
+                    } catch (ArithmeticException e2){
+                        System.out.println("El valor de a no puede ser igual a 0, y el valor de la raiz no debe ser negativa");
+                        sc.nextLine();
+                    } catch (Exception e3) {
+                        System.out.println("Error en la operation");
+                        sc.nextLine();
+                    }
+                    sc.nextLine();
                     break;
             }
         } while (opcion != 0);
+
+        sc.close();
     }
 }
