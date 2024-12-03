56917a0857a3cb8bcadd47a9fba8cf48d192f476
piioni <rangelc.juane@gmail.com>
2024-12-03 16:08:19 +0100

    Finished the pow exercise, using assertions

--- src/Unidad3/Clase/Potencia/CalcularPotencia.java
+++ src/Unidad3/Clase/Potencia/CalcularPotencia.java
@@ -3,21 +3,27 @@ package Unidad3.Clase.Potencia;
 public class CalcularPotencia {
 
     public static void potencia(int base, int exponente) {
-        assert base < 0 || base > 10 : "La base debe ser mayor a 0 y menor a 20 inlcuyente";
-        assert exponente < 0 || exponente > 10 : "El exponente debe ser mayor a 0 y menor a 20 inlcuyente";
+        // assertions para cerciorarnos que los valores de base y exponente estén dentro de los rangos permitidos.
+        assert base >= 0 && base <= 10 : "La base debe ser mayor a 0 y menor a 10 incluyente";
+        assert exponente >= 0 && exponente <= 20 : "El exponente debe ser mayor a 0 y menor a 20 incluyente";
         int resultado = 1;
+
+        // Primeros 2 casos especiales
         if (exponente == 0 || base == 1) {
             System.out.println("El resultado de la potencia es: 1");
         } else if (base == 0) {
             System.out.println("El resultado de la potencia es: 0");
-        } else if (exponente > 0 && base > 1) {
+            // Caso general de potencia
+        } else {
             for (int i = 1; i <= exponente; i++) {
                 resultado *= base;
+                // Comprobación para romper el bucle si el resultado supera 1000.
                 if (resultado > 1000) {
                     System.out.println("El resultado de la potencia sera mayor a 1000, finalizando el proceso");
                     break;
                 }
             }
+            // Comprobación de que el bucle no se haya roto para imprimir el resultado.
             if (resultado <= 1000) {
                 System.out.println("El resultado de " + base + "^" + exponente + " es igual a: " + resultado);
             }
--- src/Unidad3/Clase/Potencia/Potencia.java
+++ src/Unidad3/Clase/Potencia/Potencia.java
@@ -1,7 +1,5 @@
 package Unidad3.Clase.Potencia;
-
 import java.util.Scanner;
-
 public class Potencia {
     public static void main(String[] args) {
 
@@ -10,16 +8,20 @@ public class Potencia {
         boolean numerosCorrectos = false;
 
         Scanner sc = new Scanner(System.in);
+        // Bloque try-catch para terminar el programa si el usuario introduce un valor no numérico.
         try {
             System.out.println("Introduce la base de la potencia");
             base = Integer.parseInt(sc.nextLine());
             System.out.println("Introduce el exponente de la potencia");
             exponente = Integer.parseInt(sc.nextLine());
+            // Expresion que se evalua a true si la base y el exponente son correctos.
             numerosCorrectos = true;
         } catch (IllegalArgumentException e) {
             System.out.println("Error: " + e.getMessage());
         }
         sc.close();
+
+        // Si los números son correctos, se llama al metodo potencia con sus determinados argumentos.
         if (numerosCorrectos) {
             try{
                 CalcularPotencia.potencia(base, exponente);
