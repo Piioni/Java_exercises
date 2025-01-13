b1a2b1f20e0ce439ef3b8e9a2bee443e3e7d6d38
piioni <rangelc.juane@gmail.com>
2025-01-13 21:09:37 +0100

    Refractor leer fichero method in Fichero class

--- src/Unidad5/Fichero/Main.java
+++ src/Unidad5/Fichero/Main.java
@@ -1,25 +1,30 @@
 package Unidad5.Fichero;
 
-import java.io.FileReader;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Path path = Path.of("C:\\Users\\Usuario\\OneDrive - Instituto FOC\\Cosas FOC\\Programacion\\Programas\\src\\Unidad5\\Fichero\\fichero.txt");
-        System.out.println("Ruta absoluta: " + path.toAbsolutePath());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el nombre del fichero: ");
+        String nombreFichero = scanner.nextLine();
+        leerFichero(nombreFichero);
+    }
+
+    private static void leerFichero(String nombre) {
+        Path path = Path.of("C:\\Users\\Usuario\\OneDrive - Instituto FOC\\Cosas FOC\\Programacion\\Programas\\src\\Unidad5\\Fichero\\" + nombre);
 
         if (!Files.exists(path)) {
             System.out.println("El fichero no existe");
             return;
         }
 
-        String contenido = "";
+        String contenido;
         try {
-            contenido = Files.readString(path);
+            contenido = Files.readString(path).toUpperCase();
             System.out.println(contenido);
         } catch (IOException e) {
             System.out.println("Error al leer el fichero");
