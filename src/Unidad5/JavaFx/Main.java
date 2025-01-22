abf8a90d3e09be47a8a862962223535cb3df5466
piioni <rangelc.juane@gmail.com>
2025-01-22 21:22:29 +0100

    JavaFX

---
+++ src/Unidad5/JavaFx/Main.java
@@ -0,0 +1,21 @@
+package Unidad5.JavaFx;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("JavaFX Application");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/Unidad5/JavaFx/MainController.java
@@ -0,0 +1,13 @@
+package Unidad5.JavaFx;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainController {
+    @FXML
+    private Button MiBoton;
+
+    @FXML
+    public void hola() {
+        System.out.println("Hola");
+    }
+}
